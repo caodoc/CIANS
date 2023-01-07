c45f85ce962166ac1745c0c998f9fe2207867f1e
caodoc <dochicao2306@gmail.com>
2023-01-07 20:02:45 +0700

    Create 1238A.cpp

---
+++ Codeforces/A/1238A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int t,h,m;
+
+int main()
+{
+    cin >> t;
+    while (t--)
+    {
+        cin >> h >> m;
+        cout << (24 - h - 1) * 60 + (60 - m) << endl;
+    }
+}
