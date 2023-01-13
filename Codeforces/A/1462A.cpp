7d2d0ddf6d004901468a079a282a20eb09124a23
caodoc <dochicao2306@gmail.com>
2023-01-13 23:26:36 +0700

    Update 1462A.cpp

--- Codeforces/A/1462A.cpp
+++ Codeforces/A/1462A.cpp
@@ -12,15 +12,17 @@ int main()
         cin >> n;
         int a[n];
         for (int i = 1;i <= n;i++) cin >> a[i];
-        int t1 = 0,t2 = n - 1,ck = 0;
-        for (int i = 1;i <= n;i++) if(ck == 0)
+        int t1 = 0,t2 = n,ck = 0;
+        for (int i = 1;i <= n;i++) if (ck == 0)
         {
-            cout << a[t1++] << " ";
+            t1++;
+            cout << a[t1] << " ";
             ck = 1;
         }
         else
         {
-            cout << a[t2--] << " ";
+            cout << a[t2] << " ";
+            t2--;
             ck = 0;
         }
         cout << endl;
