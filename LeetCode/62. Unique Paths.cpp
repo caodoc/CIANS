819a3edf674f4727e23c140a9f5437be9a6e20ce
caodoc <dochicao2306@gmail.com>
2023-06-06 18:12:43 +0700

    Create 62. Unique Paths.cpp

---
+++ LeetCode/62. Unique Paths.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n,m;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> m >> n;
+    if ((m == 1 && n == 2) || (m == 2 && n == 1)) return 1;
+    if (m == 1 || n == 1) return 1;
+    long long dp[m + 7][n + 7];
+    dp[0][0] = 1;
+    for (int i = 1;i < m;++i)
+    {
+        dp[i][0] = 1;
+        for (int j = 1;j < n;++j)
+        {
+            dp[0][j] = 1;
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]; 
+        }
+    }
+    cout << dp[m - 1][n - 1];
+}
